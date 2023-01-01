62a02c9f8937acc495e1ce8b2469ec14eabaf5a9
Reza Heidari <reza.heidari@amplicade.com>
2023-01-01 22:23:22 +0300

    create event component inline

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-event',
   template: `
-    <p>
-      create-event works!
-    </p>
+    <h1></h1>
+    <hr>
+    <div class="col-md-6">
+      <h3>[Create event form will go here]</h3>
+      <br/>
+      <br/>
+      <button type="submit" class="btn btn-primary">
+        Save
+      </button>
+      <button type="submit" class="btn btn-default" (click)="onCancelClick()">
+        Cancel
+      </button>
+    </div>
   `,
   styles: [
   ]
 })
 export class CreateEventComponent {
 
+  constructor(private router: Router) {
+  }
+
+  onCancelClick() {
+    this.router.navigate(['/events']);
+  }
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from "@angular/router";
 import { EventDetailsComponent } from "./events/event-details/event-details.component";
 import { EventsListComponent } from "./events/events-list.component";
+import { CreateEventComponent } from "./events/create-event.component";
 
 export const AppRoutes: Routes = [
     { path: 'events', component: EventsListComponent },
+    { path: 'events/new', component: CreateEventComponent },
     { path: 'events/:id', component: EventDetailsComponent },
     { path: '', pathMatch: 'full', redirectTo: 'events' },
 ]
