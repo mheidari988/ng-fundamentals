fe2e2a8002be94f70dfe362cbc1c8adcb9308569
Reza Heidari <reza.heidari@amplicade.com>
2023-01-01 19:20:12 +0300

    refactor

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,21 +1,25 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'event-thumbnail',
     template: `
         <div class="well hoverwell thumbnail">
-            <h2>{{ event.name }}</h2>
-            <div>Date: {{ event.date }}</div>
-            <div>Time: {{ event.time }}</div>
-            <div>Price: \${{ event.price }}</div>
-            <div>
-                <span>Location: {{ event.location.address }}</span>
-                <span>&nbsp;</span>
-                <span>{{ event.location.city }}, {{ event.location.country }}</span>
+            <h2>{{ event?.name }}</h2>
+            <div>Date: {{ event?.date }}</div>
+            <div>Time: {{ event?.time }}</div>
+            <div>Price: \${{ event?.price }}</div>
+            <div [hidden]="!event?.location">
+                <span>Location: {{ event?.location?.address }}</span>
+                <span class="pad-left">{{ event?.location?.city }}, {{ event?.location?.country }}</span>
             </div>
-            <button class="btn btn-primary">Click me!</button>
         </div>  
-    `
+    `,
+    styles: [`
+        .thumbnail{min-height:210px;}
+        .pad-left{margin-left:10px;}
+        .well div{color:#bbb;}
+
+    `]
 })
 export class EventThumbnailComponent {
     @Input() event: any;
