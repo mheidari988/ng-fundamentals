a2c1de931e3f6155e8ddcc7cfabf084a867526e3
Reza Heidari <reza.heidari@amplicade.com>
2023-01-01 20:22:59 +0300

    add first implementation of service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavbarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/eventService';
 
 @NgModule({
   imports: [
@@ -15,6 +16,9 @@ import { NavbarComponent } from './nav/navbar.component';
     EventsListComponent,
     EventThumbnailComponent,
     NavbarComponent
+  ],
+  providers: [
+    EventService
   ],
   bootstrap: [
     EventsAppComponent
