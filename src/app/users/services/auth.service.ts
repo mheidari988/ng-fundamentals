d436108e7298cbef72c34c34b8feced9b773fa92
Reza Heidari <reza.heidari@amplicade.com>
2023-01-07 21:21:37 +0300

    update login service

--- src/app/users/services/auth.service.ts
+++ src/app/users/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { IUser } from '../models/iuser';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,15 +9,16 @@ import { IUser } from '../models/iuser';
 export class AuthService {
   currentUser?: IUser
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   loginUser(username: string, password: string) {
-    this.currentUser = {
-      id: 1,
-      userName: username,
-      firstName: 'Reza',
-      lastName: 'Heidari'
-    };
+    const options = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    const authPack = { username: username, password: password };
+    return this.http.post('/api/login', authPack, options)
+      .pipe(catchError(err => {
+        console.log(err);
+        return of(false);
+      }))
   }
 
   updateUser(firstName: string, lastName: string) {
